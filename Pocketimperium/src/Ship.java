9321083d017d36fb2d2a89917901f55ca3f59194
niksyf <99390444+niksyf@users.noreply.github.com>
2024-12-24 22:06:35 +0800

    Update Ship.java

--- Pocketimperium/src/Ship.java
+++ Pocketimperium/src/Ship.java
@@ -4,6 +4,14 @@ class Ship {
     private Player owner;
     private Sector location;
 
+    public Ship(Player owner) {
+        this.owner = owner;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
     public void moveTo(Sector sector) {
         this.location = sector;
     }
