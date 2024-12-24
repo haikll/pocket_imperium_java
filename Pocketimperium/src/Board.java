d92bd6bf15c166b6db605944e2733e22da12b033
niksyf <99390444+niksyf@users.noreply.github.com>
2024-12-24 22:01:14 +0800

    Update Board.java

--- Pocketimperium/src/Board.java
+++ Pocketimperium/src/Board.java
@@ -5,6 +5,7 @@ class Board {
     private static Board instance;
     private List<Sector> sectors;
 
+
     public Board() {
         this.sectors = new ArrayList<>();
     }
@@ -17,26 +18,25 @@ class Board {
     }
 
     public void setup() {
-        int[][] sectorLevels = {
-            {2, 0, 0, 1, 0, 1, 0, 0, 0},
-            {0, 0, 1, 0, 2, 0, 2, 1, 0},
-            {0, 1, 0, 0, 0, 0, 0, 0, 2},
-            {0, 0, 0, 0, 0, 0, 0, 0, 1},
-            {2, 1, 0, 0, 3, 0, 0, 2, 0},
-            {0, 0, 2, 0, 0, 0, 1, 0, 0},
-            {0, 1, 0, 0, 0, 0, 0, 2, 0},
-            {1, 0, 0, 1, 0, 1, 0, 0, 0},
-            {0, 0, 2, 0, 2, 0, 1, 0, 1}
-        };
+    int[][] sectorLevels = {
+    	{2, 0, 0, 1, 0, 1, 0, 0, 0},
+        {0, 0, 1, 0, 2, 0, 2, 1, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 2},
+        {0, 0, 0, 3, 3, 3, 0, 0, 1},
+        {2, 1, 0, 3, 3, 3, 0, 2, 0},
+        {0, 0, 2, 3, 3, 3, 1, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0, 2, 0},
+        {1, 0, 0, 1, 0, 1, 0, 0, 0},
+        {0, 0, 2, 0, 2, 0, 1, 0, 1}
+    };
 
-        for (int row = 0; row < sectorLevels.length; row++) {
-            for (int col = 0; col < sectorLevels[row].length; col++) {
-                if (sectorLevels[row][col] != 0) {
-                    sectors.add(new Sector(sectorLevels[row][col]));
-                }
-            }
+    for (int row = 0; row < sectorLevels.length; row++) {
+        for (int col = 0; col < sectorLevels[row].length; col++) {
+            String sectorName = Character.toString('A' + row) + (col + 1); // Generate names like A1, A2, ...
+            sectors.add(new Sector(sectorLevels[row][col], sectorName));
         }
     }
+                }
 
 
     public List<Sector> getSectors() {
