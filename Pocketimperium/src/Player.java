273f44fe608b250044cd7c921d1f6983ccef90c9
niksyf <99390444+niksyf@users.noreply.github.com>
2024-12-24 22:05:40 +0800

    Update Player.java

--- Pocketimperium/src/Player.java
+++ Pocketimperium/src/Player.java
@@ -31,17 +31,28 @@ class Player {
             strategy.decideCommandOrder();
         } else {
             System.out.println(name + ", choose the order of your commands (1 for Expand, 2 for Explore, 3 for Exterminate):");
-            for (int i = 0; i < 3; i++) {
-                System.out.println("Enter command " + (i + 1) + ": ");
-                int commandIndex = scanner.nextInt();
-                commands.add(availableCommands.get(commandIndex - 1));
+            while (commands.size() < 3) {
+                try {
+                    System.out.println("Enter command " + (commands.size() + 1) + ": ");
+                    int commandIndex = scanner.nextInt();
+                    if (commandIndex < 1 || commandIndex > 3) {
+                        System.out.println("Invalid choice. Please enter 1, 2, or 3.");
+                        continue;
+                    }
+                    commands.add(availableCommands.get(commandIndex - 1));
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter a number.");
+                    scanner.next(); // Clear invalid input
+                }
             }
         }
     }
 
-    public void executeCommand(Command command) {
-        command.execute(this, Board.getInstance());
+
+    public void executeCommand(Command command, Game game) {
+        command.execute(this, Board.getInstance(), game);
     }
+
 
     public int calculateScore() {
         return score;
@@ -61,30 +72,40 @@ class Player {
 
     public void scoreSector(Board board) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Available sectors:");
         List<Sector> sectors = board.getSectors();
-        for (int i = 0; i < sectors.size(); i++) {
-            Sector sector = sectors.get(i);
+
+        System.out.println("Available sectors:");
+        for (Sector sector : sectors) {
             String ownerName = (sector.getOwner() != null) ? sector.getOwner().getName() : "Unoccupied";
-            System.out.println(i + ": Level " + sector.getLevel() + ", Owner: " + ownerName);
+            System.out.println(sector.getName() + ": Level " + sector.getLevel() + ", Owner: " + ownerName);
         }
 
-        System.out.println("Choose a sector to score (enter index):");
-        int sectorIndex = scanner.nextInt();
+        System.out.println("Choose a sector to score (enter the name, e.g., A1):");
+        String sectorName = scanner.nextLine();
 
-        if (sectorIndex < 0 || sectorIndex >= sectors.size()) {
-            System.out.println("Invalid sector index. Please try again.");
+        Sector chosenSector = null;
+        for (Sector sector : sectors) {
+            if (sector.getName().equalsIgnoreCase(sectorName)) {
+                chosenSector = sector;
+                break;
+            }
+        }
+
+        if (chosenSector == null) {
+            System.out.println("Invalid sector name. Please try again.");
             return;
         }
 
-        Sector chosenSector = sectors.get(sectorIndex);
         if (chosenSector.getOwner() != null && chosenSector.getOwner().equals(this)) {
-            score += chosenSector.getLevel(); // Include Level 0 sectors
-            System.out.println(name + " scored " + chosenSector.getLevel() + " points!");
+            score += chosenSector.getLevel();
+            System.out.println(name + " scored " + chosenSector.getLevel() + " points from " + chosenSector.getName() + "!");
         } else {
             System.out.println("Invalid sector selection or you don't own it.");
         }
     }
+
+
+
 
 
     @Override
